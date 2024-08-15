53adb95373db89e717071ae93b6a48c92a180aac
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-15 14:24:03 -0300

    :lipstick: feat: Adicionando melhorias na impressão do tabuleiro

--- src/chess/PartidaXadrez.java
+++ src/chess/PartidaXadrez.java
@@ -29,8 +29,18 @@ public class PartidaXadrez {
     }
 
     private void iniciarJogo() {
-        colocarNovaPeca('c', 6, new Torre(tabuleiro, Cor.BLACK));
-        colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.BLACK));
-        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.WHITE));
+        colocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.WHITE));
+        colocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.WHITE));
+        colocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.WHITE));
+        colocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.WHITE));
+        colocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.WHITE));
+        colocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.WHITE));
+
+        colocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.BLACK));
+        colocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.BLACK));
+        colocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.BLACK));
+        colocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.BLACK));
+        colocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.BLACK));
+        colocarNovaPeca('d', 8, new Rei(tabuleiro, Cor.BLACK));
     }
 }
