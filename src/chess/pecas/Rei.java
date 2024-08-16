eecaaf7c12936e7f011884f68ba505396152c5f5
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-16 16:51:50 -0300

    :lipstick: feat: Adicionando movimentos possíveis do rei

--- src/chess/pecas/Rei.java
+++ src/chess/pecas/Rei.java
@@ -1,5 +1,6 @@
 package chess.pecas;
 
+import boardgame.Posicao;
 import boardgame.Tabuleiro;
 import chess.PecaXadrez;
 import chess.enums.Cor;
@@ -15,9 +16,65 @@ public class Rei extends PecaXadrez {
         return "R";
     }
 
+    private boolean podeMover (Posicao posicao) {
+        PecaXadrez pecaXadrez = (PecaXadrez) getTabuleiro().peca(posicao);
+        return pecaXadrez == null || pecaXadrez.getCor() != getCor();
+    }
+
     @Override
     public boolean[][] possiveisMovimentos() {
         boolean[][] movimentos = new boolean[getTabuleiro().getRows()][getTabuleiro().getCols()];
+
+        Posicao posicaoAux = new Posicao(0, 0);
+
+        // Acima da peca
+        posicaoAux.setValue(posicao.getRow() - 1, posicao.getColumn());
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Abaixo da peca
+        posicaoAux.setValue(posicao.getRow() + 1, posicao.getColumn());
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Esquerda da peca
+        posicaoAux.setValue(posicao.getRow(), posicao.getColumn() - 1);
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Direita da peca
+        posicaoAux.setValue(posicao.getRow(), posicao.getColumn() + 1);
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Diagonal superior esquerda da peca
+        posicaoAux.setValue(posicao.getRow() - 1, posicao.getColumn() - 1);
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Diagonal superior direita da peca
+        posicaoAux.setValue(posicao.getRow() - 1, posicao.getColumn() + 1);
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Diagonal inferior esquerda da peca
+        posicaoAux.setValue(posicao.getRow() + 1, posicao.getColumn() - 1);
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
+        // Diagonal inferior direita da peca
+        posicaoAux.setValue(posicao.getRow() + 1, posicao.getColumn() + 1);
+        if (getTabuleiro().posicaoExiste(posicaoAux) && podeMover(posicaoAux)) {
+            movimentos[posicaoAux.getRow()][posicaoAux.getColumn()] = true;
+        }
+
         return movimentos;
     }
 }
